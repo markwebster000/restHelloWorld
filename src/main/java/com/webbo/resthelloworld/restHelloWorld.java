c6eac7ba22ec2f89310e5dbb701efa76cd3f7845
Mark Webster <mark@iMac-G5.local>
2017-03-10 14:51:29 +1100

    Initial commit

---
+++ src/main/java/com/webbo/resthelloworld/restHelloWorld.java
@@ -0,0 +1,18 @@
+package com.webbo.resthelloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
+public class restHelloWorld {
+  // The Java method will process HTTP GET requests
+  @GET
+  // The Java method will produce content identified by the MIME Media type "text/plain"
+  @Produces("text/plain")
+  public String getMessage() {
+    // Return some cliched textual content
+    return "Hello World";
+  }
+}
