f647294909b63cd55162314d9a999dba60902ae1
mark <markwebster000@yahoo.com.au>
2017-03-10 15:12:47 +1100

    Changed helloworld to hellotext

--- src/main/java/com/webbo/resthelloworld/restHelloWorld.java
+++ src/main/java/com/webbo/resthelloworld/restHelloWorld.java
@@ -4,14 +4,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-// The Java class will be hosted at the URI path "/helloworld"
-@Path("/helloworld")
+// The Java class will be hosted at the URI path "/hellotext"
+@Path("/hellotext")
 public class restHelloWorld {
   // The Java method will process HTTP GET requests
   @GET
   // The Java method will produce content identified by the MIME Media type "text/plain"
   @Produces("text/plain")
-  public String getMessage() {
+  public String getMessageText() {
     // Return some cliched textual content
     return "Hello World";
   }
