c4ad9cdaadfba37d4a54176a55500f8ee73e4c54
mark <markwebster000@yahoo.com.au>
2017-03-10 15:19:49 +1100

    Changed text/plain to MediaType.TEXT_PLAIN

--- src/main/java/com/webbo/resthelloworld/restHelloWorld.java
+++ src/main/java/com/webbo/resthelloworld/restHelloWorld.java
@@ -3,6 +3,7 @@ package com.webbo.resthelloworld;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 // The Java class will be hosted at the URI path "/hellotext"
 @Path("/hellotext")
@@ -10,7 +11,7 @@ public class restHelloWorld {
   // The Java method will process HTTP GET requests
   @GET
   // The Java method will produce content identified by the MIME Media type "text/plain"
-  @Produces("text/plain")
+  @Produces(MediaType.TEXT_PLAIN)
   public String getMessageText() {
     // Return some cliched textual content
     return "Hello World";
