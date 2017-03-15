b02ac8b387c7f74ea88ca795ff312047be3f9ad9
mark <markwebster000@yahoo.com.au>
2017-03-15 11:40:47 +1100

    Changed from returning text to returning JSON.

---
+++ src/main/java/com/webbo/resthelloworld/TestMessage.java
@@ -0,0 +1,15 @@
+package com.webbo.resthelloworld;
+
+/**
+ * Created by mark on 15/3/17.
+ */
+public class TestMessage {
+    private String testMessage;
+
+    public void setTestMessage(String testMessage) {
+        this.testMessage = testMessage;
+    }
+    public String getTestMessage() {
+        return testMessage;
+    }
+}
--- src/main/java/com/webbo/resthelloworld/restHelloWorld.java
+++ src/main/java/com/webbo/resthelloworld/restHelloWorld.java
@@ -5,15 +5,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-// The Java class will be hosted at the URI path "/hellotext"
+
 @Path("/hellotext")
 public class restHelloWorld {
-  // The Java method will process HTTP GET requests
+
   @GET
-  // The Java method will produce content identified by the MIME Media type "text/plain"
-  @Produces(MediaType.TEXT_PLAIN)
-  public String getMessageText() {
-    // Return some cliched textual content
-    return "Hello World";
+  @Produces(MediaType.APPLICATION_JSON)
+  public TestMessage getMessageText() {
+      TestMessage testMessage = new TestMessage();
+      testMessage.setTestMessage("Hello World!");
+    return testMessage;
   }
 }
